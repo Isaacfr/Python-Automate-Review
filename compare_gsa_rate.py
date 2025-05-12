18e1e0151d9d8f736d343491a3d857bc5385c7c0
Isaac Frias <ifrias@csicompanies.com>
2025-05-12 15:02:20 -0400

    Fixed a bug with Blank or None datatypes from Excel Sheet

--- compare_gsa_rate.py
+++ compare_gsa_rate.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-def compare_gsa_rate_function(year = 2025, zip_code = 20001, check_month='3'):
+def compare_gsa_rate_function(year = 2025, zip_code = 32065, check_month='3'):
 
    #Utilize URL for web-scrapping
    url = f'https://www.gsa.gov/travel/plan-book/per-diem-rates/per-diem-rates-results?action=perdiems_report&fiscal_year={year}&state=&city=&zip={zip_code}'
@@ -32,60 +32,64 @@ def compare_gsa_rate_function(year = 2025, zip_code = 20001, check_month='3'):
 
    #For this website, find the specific table and class to extract information
    table = soup.find('table', class_ = 'table_perdiem')
-
-   #Testing to make sure identity is correct
-   #table = soup.find('table', class_ = 'table_perdiem stripedTable dataTable no-footer dtr-inline')
-   #table = soup.find_all('table')[1]
-   #print(table)
-
-   #Extract Headers
-   world_titles = table.find_all('th')
-   world_table_titles = [title.text.strip().replace('\xa0', '') for title in world_titles]
-
-   #Testing values and what kind of data is being extracted
-   #print(world_table_titles)
-   #months = dict.fromkeys(world_table_titles[2:], None)
-   months = world_table_titles[2:]
-   new_months = []
-
-   #Some data includes a year as well, this is to extract the 3-letter date
-   for month in months:
-         month_str = month[-3:].lower()
-         new_months.append(month_mapping[month_str])
-
-   #Testing difference
-   #print(months)
-   #print(new_months)
-
-   #Create a dataframe object to extract data
-   df = pd.DataFrame(columns = new_months)
-
-   #Find all the rows and insert them into the dataframe
-   #Create a blank dictionary for month data
-   column_data = table.find_all('tr')
-   months_dict = {}
-
-   #Reiterate to each data and extract data, besides headers. 
-   for row in column_data[1:]:
-        row_data = row.find_all('td')
-        individual_row_data = [data.text.strip() for data in row_data[2:]]
-    
-        #print(individual_row_data)
-        length = len(df)
-    
-        df.loc[length] = individual_row_data
-
-   row_index = 0
-   months_dict = df.iloc[row_index].to_dict()
-
-   #print(df)
-   #print(months_dict)
+   if table is not None:
    
-   #Extract rate based on month that was used as input
-   lodging_rate = months_dict[check_month]
-   #print(lodging_rate)
+       #Testing to make sure identity is correct
+       #table = soup.find('table', class_ = 'table_perdiem stripedTable dataTable no-footer dtr-inline')
+       #table = soup.find_all('table')[1]
+       #print(table)
+
+       #Extract Headers
+       world_titles = table.find_all('th')
+       world_table_titles = [title.text.strip().replace('\xa0', '') for title in world_titles]
+      
+       #Testing values and what kind of data is being extracted
+       #print(world_table_titles)
+       #months = dict.fromkeys(world_table_titles[2:], None)
+       months = world_table_titles[2:]
+       new_months = []
+
+       #Some data includes a year as well, this is to extract the 3-letter date
+       for month in months:
+             month_str = month[-3:].lower()
+             new_months.append(month_mapping[month_str])
+
+       #Testing difference
+       #print(months)
+       #print(new_months)
+
+       #Create a dataframe object to extract data
+       df = pd.DataFrame(columns = new_months)
+
+       #Find all the rows and insert them into the dataframe
+       #Create a blank dictionary for month data
+       column_data = table.find_all('tr')
+       months_dict = {}
+
+       #Reiterate to each data and extract data, besides headers. 
+       for row in column_data[1:]:
+            row_data = row.find_all('td')
+            individual_row_data = [data.text.strip() for data in row_data[2:]]
+    
+            #print(individual_row_data)
+            length = len(df)
+    
+            df.loc[length] = individual_row_data
+
+       row_index = 0
+       months_dict = df.iloc[row_index].to_dict()
+
+       #print(df)
+       #print(months_dict)
    
-   return lodging_rate
+       #Extract rate based on month that was used as input
+       lodging_rate = months_dict[check_month]
+       #print(lodging_rate)
+   
+       return lodging_rate
+
+   else:
+       return 0
 
 #Test calling the function
 #compare_gsa_rate_function()
--- run_third_check.py
+++ run_third_check.py
@@ -1,7 +1,8 @@
-import filenames, filter_blanks_and_wrongids, find_closest_project, compare_duplicates, create_gsa_rate_tab
+#import filenames, filter_blanks_and_wrongids, find_closest_project, compare_duplicates, create_gsa_rate_tab
+import create_gsa_rate_tab
 
-find_closest_project.find_closest_project(filenames.source_file, filenames.source_sheet, filenames.lookup_file_path, filenames.lookup_table_file, filenames.lookup_full_path, filenames.lookup_table_sheet_name)
+#find_closest_project.find_closest_project(filenames.source_file, filenames.source_sheet, filenames.lookup_file_path, filenames.lookup_table_file, filenames.lookup_full_path, filenames.lookup_table_sheet_name)
 
-compare_duplicates.highlight_duplicates(filenames.source_file)
+#compare_duplicates.highlight_duplicates(filenames.source_file)
 
 create_gsa_rate_tab.main()
